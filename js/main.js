d5556a6f66d8d6e85124e73fd4c0cf41506edb69
HugoVeraGarcia <hugo.vera.garcia@gmail.com>
2022-06-26 12:02:44 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+const colors = ["#CB51EE", "#0073BC", "#38B75E", "#DE365C"];
+
+const figures = () => {
+  for (let i = 0; i <= 25; i++) {
+    let figure = document.createElement("span");
+    let select = Math.round(colors.length * Math.random());
+
+    figure.style.top = innerHeight * Math.random() + "px";
+    figure.style.left = innerWidth * Math.random() + "px";
+    figure.style.background =
+      colors[select >= colors.length ? select - 1 : select];
+
+    container.appendChild(figure);
+
+    setInterval(() => {
+      figure.style.top = innerHeight * Math.random() + "px";
+      figure.style.left = innerWidth * Math.random() + "px";
+    }, 10000);
+  }
+};
+
+figures();
